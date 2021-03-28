65eb1301f9ad68b2c38f0f71c201fb031d1190fb
paul <piankin250@gmail.com>
2021-03-28 10:20:01 +0300

    Rally

--- Draft/LoadInfoFromFile.py
+++ Draft/LoadInfoFromFile.py
@@ -1,6 +1,6 @@
-l = []
-with open(r"C:\Users\User\Desktop\!temp") as f:
-    content = f.readlines()
+params = []
+with open(r'C:\Users\User\Desktop\!temp') as my_file:
+    for line in my_file:
+        params.append(line)
 
-l = 
 y=6
