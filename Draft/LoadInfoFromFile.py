5ba3ac775ed13fb755df9b33118f406b2958d9e8
paul <piankin250@gmail.com>
2021-03-31 21:59:27 +0300

    Rally

--- Draft/LoadInfoFromFile.py
+++ Draft/LoadInfoFromFile.py
@@ -1,6 +1,20 @@
-params = []
-with open(r'C:\Users\User\Desktop\!temp') as my_file:
-    for line in my_file:
-        params.append(line)
+import pickle
+
+class TestCase():
+    def __init__(self, formattedID, name, preConditions, inputs, expecteds):
+        self.formattedID = formattedID
+        self.name = name
+        self.preConditions = preConditions
+        self.inputs = inputs
+        self.expecteds = expecteds
+
+l = None
+with open(r'C:\Temp2\New folder\DataVisualizationAndStatisticsForRally\Draft\listTestCases.data', 'rb') as file:
+    l = pickle.load(file)
+    t=0
+
+print(len(l))
+
+query = input("Enter query: \n")
 
 y=6
