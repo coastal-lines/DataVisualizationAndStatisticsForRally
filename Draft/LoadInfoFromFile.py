4ccd5cdb8fa9a4853792371c9a30b8fb8b8490ff
paul <piankin250@gmail.com>
2021-04-02 18:06:52 +0300

    Rally

--- Draft/LoadInfoFromFile.py
+++ Draft/LoadInfoFromFile.py
@@ -1,4 +1,5 @@
 import pickle
+import re
 
 class TestCase():
     def __init__(self, formattedID, name, preConditions, productArea, productSubarea, method, testFolder, inputs, expecteds):
@@ -13,13 +14,67 @@ class TestCase():
         self.inputs = inputs
         self.expecteds = expecteds
 
-l = None
-with open(r'C:\Temp2\New folder\DataVisualizationAndStatisticsForRally\Draft\listTestCases.data', 'rb') as file:
-    l = pickle.load(file)
-    t=0
+userTestCasesFromFile = None
+with open(r'C:\Temp2\New folder\DataVisualizationAndStatisticsForRally\Draft\listTestCases2.data', 'rb') as file:
+    userTestCasesFromFile = pickle.load(file)
 
-print(len(l))
+query = '(Name contains "material")'
+query = '((Name contains "material") AND (PreConditions contains "subjec"))'
+what = None
+how = None
+where = None
 
-query = input("Enter query: \n")
+result = re.findall('\((.*?)\)', query)
+selectedUserTestCases = []
 
-y=6
+userTestCasesFromFileCopy = userTestCasesFromFile.copy()
+#pre-removing test cases
+for query in result:
+    what = query.split()[0]
+    action =  query.split()[1]
+    text = query.split()[2]
+    if(action == "!contains" or action == "!="):
+        removeTestCasesFromList()
+
+def removeTestCasesFromList(what, text):
+    for tc in userTestCasesFromFile:
+        if what == "Name":
+            pass
+        elif what == "PreCondition":
+            pass
+        elif what == "Project":
+            pass
+        elif what == "":
+            pass
+        elif what == "":
+            pass
+        elif what == "":
+            pass
+        elif what == "":
+            pass
+        elif what == "":
+            pass
+        elif what == "":
+            pass
+
+for query in result:
+    what = query.split()[0]
+    action =  query.split()[1]
+    text = query.split()[2]
+
+
+
+    for tc in userTestCasesFromFile:
+        if where == "Name":
+            pass
+
+def findByName(action, text):
+    if "contains" in action:
+        for tc in userTestCasesFromFile:
+            if text in tc.name:
+                selectedUserTestCases.append(tc)
+
+    if "=" in action:
+        for tc in userTestCasesFromFile:
+            if text == tc.name:
+                selectedUserTestCases.append(tc)
