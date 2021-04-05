9cc600b1d45d4ccbf53b73692c914418da40a71b
paul <piankin250@gmail.com>
2021-04-05 23:32:20 +0300

    Rally

--- Project/RallyTestCase.py
+++ Project/RallyTestCase.py
@@ -1,15 +1,16 @@
 class TestCase():
     #def __init__(self, formattedID, name, preConditions, inputs, expecteds):
-    def __init__(self, formattedID, name, preConditions, inputs, expecteds, mainFolderName, rootFolderName, productArea, productSubarea, method, testFolder):
-        self.mainFolderName = mainFolderName
-        self.rootFolderName = rootFolderName
+    def __init__(self, formattedID, name, preConditions, inputs, expecteds, mainFolderName, rootFolderName, project, productArea, productSubarea, method, testFolder, isAutomated):
         self.formattedID = formattedID
         self.name = name
         self.preConditions = preConditions
+        self.inputs = inputs
+        self.expecteds = expecteds
+        self.mainFolderName = mainFolderName
+        self.rootFolderName = rootFolderName
         self.project = project
         self.productArea = productArea
         self.productSubarea = productSubarea
         self.method = method
         self.testFolder = testFolder
-        self.inputs = inputs
-        self.expecteds = expecteds
+        self.isAutomated = isAutomated
